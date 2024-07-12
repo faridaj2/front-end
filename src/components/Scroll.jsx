eebc85fc8b02bb6809572317a37a99ad1745a38e
faridaj2 <ajfarid6@gmail.com>
2024-07-12 14:07:21 +0700

    finish, fix scroll bug

--- src/components/Scroll.jsx
+++ src/components/Scroll.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
 
 function Scroll({ children }) {
+    const divRef = useRef();
+
+    const handleClickRef = () => {
+        divRef.current.click();
+    };
+    useEffect(() => {
+        handleClickRef()
+    }, [])
+
     return (
-        <div className='h-screen overflow-y-auto scroll mx-3'>
+        <div className='h-screen overflow-y-auto scroll mx-3' ref={divRef}>
             {children}
             <div className='h-24'>
-
+                <button onClick={handleClickRef} style={{ display: 'none' }}>Klik Ref</button>
             </div>
         </div>
-    )
+    );
 }
 
-export default Scroll
+export default Scroll;
